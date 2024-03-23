2acfef1147f24b5d54ca82b4323b088954f38397
Hughgz <minhhieu.swe@gmail.com>
2024-03-24 03:37:13 +0700

    commit

---
+++ src/main/java/com/example/minhhieu/LearnToeicSpring/LearnToeicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.minhhieu.LearnToeicSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToeicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnToeicSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minhhieu/LearnToeicSpring/LearnToeicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minhhieu.LearnToeicSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnToeicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
